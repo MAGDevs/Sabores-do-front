7d319a9322804ec76ad360bce0910e9732d7c80d
Gaby Bonfim <gabrielybonfim05@gmail.com>
2025-10-23 09:24:40 -0300

    TesteM

--- CP5-RECEITAS/src/Componentes/Cabecalho/Cabecalho.tsx
+++ CP5-RECEITAS/src/Componentes/Cabecalho/Cabecalho.tsx
@@ -4,8 +4,7 @@ const Cabecalho = () => {
   const { pathname } = useLocation();
 
   return (
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
+
     <header className="bg-pink-400 text-white shadow-lg">
       <div className="container mx-auto px-4 py-3">
         <div className="flex justify-between items-center">
@@ -19,19 +18,14 @@ const Cabecalho = () => {
             <Link to="/login" className="hover:text-pink-700 transition">Login</Link>
           </nav>
         </div>
-=======
-=======
->>>>>>> Stashed changes
+
     <header className="bg-[#f0ffe9] flex justify-between items-center px-10 py-6 shadow-md">
       <div className="flex items-center gap-3">
         <img src="/logo.png" alt="Logo Sabores do Front" className="w-10 h-10" />
         <h1 className="text-gray-800 text-xl font-semibold">
           &lt;SABORES DO FRONT/&gt;
         </h1>
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
+
       </div>
 
       <nav className="flex gap-6 font-semibold">
@@ -61,7 +55,9 @@ const Cabecalho = () => {
         </Link>
       </nav>
     </header>
-  );
+  )
+
+  
 };
 
 export default Cabecalho;
